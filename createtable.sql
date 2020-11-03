29d9395f05641f98ab24799913b4574a6e6b1267
Jessica Mendoza <mendoza.jy@gmail.com>
2020-11-02 21:29:36 -0800

    uploading

---
+++ createtable.sql
@@ -0,0 +1,9 @@
+create table walmartdata (
+item_number VARCHAR(500) PRIMARY KEY NOT NULL,
+product_name VARCHAR(1000),
+product_url VARCHAR (1000),
+price_2019 FLOAT,
+price_2020 FLOAT,
+brand VARCHAR(1000),
+category VARCHAR(1000)
+)
