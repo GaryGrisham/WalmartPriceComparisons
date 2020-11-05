0fa61d4d70c9cc1d12ef59d10b95910a0f0783d5
Maria Smith <smith.maria.b@gmail.com>
2020-11-05 15:44:27 -0800

    updated flask app

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from sqlalchemy.sql import text
 from flask import Flask, jsonify
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import numpy as np
-import datetime as dt
-app = Flask(__name__)
+import json
+
 
 # create engine
 engine = create_engine('postgresql://admin22:12345@localhost:5432/saccrewproject2')
@@ -15,16 +14,16 @@ Base = automap_base()
 Base.prepare(engine, reflect = True)
 session = Session(engine)
 
+
+app = Flask(__name__)
 @app.route('/')
-def testdb():
-    try:
-        engine.execute('SELECT * FROM walmartdata')
-        return '<h1>It works.</h1>'
-    except Exception as e:
-        # e holds description of the error
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+
+
+def jsonifyData():
+    results = engine.execute('SELECT * FROM walmartdata').fetchall()
+
+    return jsonify({'result': [dict(row) for row in results]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
