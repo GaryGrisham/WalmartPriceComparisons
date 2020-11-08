93fe0bf2857bdd2ae76bc699d850923a3e988462
Maria Smith <smith.maria.b@gmail.com>
2020-11-07 22:05:54 -0800

    add columns

--- createtable.sql
+++ createtable.sql
@@ -1,9 +1,13 @@
-create table walmartdata (
+DROP TABLE walmartdata
+
+CREATE TABLE walmartdata (
 item_number VARCHAR(500) PRIMARY KEY NOT NULL,
 product_name VARCHAR(1000),
 product_url VARCHAR (1000),
 price_2019 FLOAT,
 price_2020 FLOAT,
+price_difference FLOAT,
+price_percent_change FLOAT,
 brand VARCHAR(1000),
 category VARCHAR(1000)
 )
