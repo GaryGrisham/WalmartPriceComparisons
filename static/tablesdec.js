e45c3e71ffadedcbe316d02f713166d3604d55c2
GaryGrisham <67010627+GaryGrisham@users.noreply.github.com>
2020-11-11 08:00:25 -0800

    updates to dec

--- static/tablesdec.js
+++ static/tablesdec.js
@@ -5,16 +5,16 @@ d3.json(query).then((data) => {
 
 var tbody = d3.select("tbody");
 
-// Console.log the inc data from data.js
+// Console.log the dec data from data.js
 // console.log(waldata);
 
 // Step 1: Loop Through `data` and console.log each object
-data.forEach(function(inc){
-    console.log(inc)
+data.forEach(function(dec){
+    console.log(dec)
     var row = tbody.append('tr');
     var row2 = tbody.append('tr');
 
-    Object.entries(inc).forEach( function([x,y]){
+    Object.entries(dec).forEach( function([x,y]){
         console.log(`x is ${x} y is ${y}`)
         row.append('td').text(y);
         row2.append('td').text('    ')
