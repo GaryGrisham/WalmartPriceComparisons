d06ce59e0cee7cdc0ce4b029ec5e34b01c1e9084
GaryGrisham <67010627+GaryGrisham@users.noreply.github.com>
2020-11-10 19:38:37 -0800

    some gary code

---
+++ static/tablesInc.js
@@ -0,0 +1,23 @@
+var query = "http://127.0.0.1:5000/api/toptenpriceincrease"
+
+d3.json(query).then((data) => {
+    console.log(data);
+
+var tbody = d3.select("tbody");
+
+// Console.log the inc data from data.js
+// console.log(waldata);
+
+// Step 1: Loop Through `data` and console.log each object
+data.forEach(function(inc){
+    console.log(inc)
+    var row = tbody.append('tr');
+    var row2 = tbody.append('tr');
+
+    Object.entries(inc).forEach( function([x,y]){
+        console.log(`x is ${x} y is ${y}`)
+        row.append('td').text(y);
+        row2.append('td').text('    ')
+    })
+
+});
