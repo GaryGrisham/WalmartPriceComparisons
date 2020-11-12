9a436458743babf5625b08fb7f4554499029dcb2
Maria Smith <smith.maria.b@gmail.com>
2020-11-11 18:08:03 -0800

    box and whisker

--- static/boxAndWhisker.js
+++ static/boxAndWhisker.js
@@ -1,5 +1,15 @@
 window.onload = function () {
-    var query = "http://127.0.0.1:5000/api/categorycounts";
+    var query = 'http://127.0.0.1:5000/api/datacall';
+
+    function QuartCalc(d) {
+        q1 = d3.quantile(d.map(function(g) { return g;}).sort(d3.ascending),.25)
+        median = d3.quantile(d.map(function(g) { return g;}).sort(d3.ascending),.5)
+        q3 = d3.quantile(d.map(function(g) { return g;}).sort(d3.ascending),.75)
+        interQuantileRange = q3 - q1
+        min = q1 - 1.5 * interQuantileRange
+        max = q3 + 1.5 * interQuantileRange
+        return([min, q1, q3, max, median])
+      }
 
     // Import Data
     d3.json(query).then(function(data) {
@@ -11,8 +21,41 @@ window.onload = function () {
         for (var i = 0; i < data.length; i++) {
             AllCategories.push(data[i].category);
         }
-          console.log(AllCategories);
-          categories = AllCategories.filter(UniqueVal);
+        categories = AllCategories.filter(UniqueVal);
+        console.log(categories);
+
+        
+    var plotData = []
+    var  outliers = []
+    for (let i = 0; i < categories.length ; i++) {
+        var dataDict = {};
+        categoryPriceChange = [];
+       
+        
+        for (let j = 0; j < data.length; j++){
+            if (data[j].category == categories[i]){
+                categoryPriceChange.push(data[j].price_percent_change);
+            }    
+          };
+        dataDict['x'] = i;
+        dataDict["label"] = categories[i];
+        dataDict['y'] = QuartCalc(categoryPriceChange);
+        plotData.push(dataDict);
+        for (k=0; k < categoryPriceChange.length; k ++){
+            if((categoryPriceChange[k] > QuartCalc(categoryPriceChange)[3]) ||  (categoryPriceChange[k] < QuartCalc(categoryPriceChange)[0])){
+                outliers.push({'x': i, 'label': categories[i], 'y': categoryPriceChange[k]});
+
+            };
+        };
+        };
+
+
+
+
+
+    
+console.log(plotData);
+
 
     var options = {
         animationEnabled: true,
@@ -38,37 +81,17 @@ window.onload = function () {
         data: [{
             type: "boxAndWhisker",
             toolTipContent: "<span style=\"color:#6D78AD\">{label}:</span> <br><b>Upper Extreme:</b> {y[3]},<br><b>Q3:</b> {y[2]},<br><b>Median:</b> {y[4]}<br><b>Q1:</b> {y[1]}<br><b>Lower Extreme:</b> {y[0]}",
-            dataPoints: [
-                { x: 0, label: "Sports & Outdoors", y: [-124.25, 46, 159.5, 329.75, 124.5] },
-                { x: 1, label: "Food ", y: [-226.5, 0, 151, 377.5, 123] },
-                { x: 2, label: "Pets ", y: [-330, 0, 220, 550, 75.5] },
-                { x: 3, label: "Party & Occasions ", y: [-191.25, 0, 127.5, 318.75, 55] },
-                { x: 4, label: "Premium Beauty ", y: [-20, 211, 365, 596, 346] },
-                { x: 0, label: "Beauty ", y: [-124.25, 46, 159.5, 329.75, 124.5] },
-                { x: 1, label: "Health ", y: [-226.5, 0, 151, 377.5, 123] },
-                { x: 2, label: "Personal Care", y: [-330, 0, 220, 550, 75.5] },
-                { x: 3, label: "Baby ", y: [-191.25, 0, 127.5, 318.75, 55] },
-                { x: 4, label: "Patio & Garden", y: [-20, 211, 365, 596, 346] }
-                
-                // x: for (var i=0; i < categories.length; i++){
-                //     console.log(i);
-                //     console.log(categories[i]);
-                //     x: i, label: categories[i], y: 
-                // }
-            ],
+            dataPoints: plotData,         
                 
         },
         {
             type: "scatter",
             name: "Outlier Values",
             color: "#F15278",
-            markerType: "cross",
+            markerType: "circle",
             toolTipContent: "<span style=\"color:#C0504E\">{county}</span>: {y} Days",
             showInLegend: true,
-            dataPoints: [
-                { x: 0, label: "California", y: 355, county: "Plumas" },
-                { x: 3, label: "New York", y: 351, county: "Nassau" }
-            ]
+            dataPoints:  outliers
         }]
 
     };
