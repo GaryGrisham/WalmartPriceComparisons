d7b035c7b9425557e643cff219627ce5b63cc29f
Maria Smith <smith.maria.b@gmail.com>
2020-11-14 10:38:16 -0800

    updates

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-# from sqlalchemy.sql import text
-# from flask import Flask, jsonify, render_template
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-# import numpy as np
-# import json
 
-# Dependencies
 import sqlalchemy
 from sqlalchemy.sql import text
-#auto map module used for mapping sql tables
 from sqlalchemy.ext.automap import automap_base
-#session command is used to create a session to the database
 from sqlalchemy.orm import Session
-#create_engine allows us to connect to the sql server, func is used to do mathmatical functions
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template, redirect
-
 
 # create engine
 engine = create_engine('postgresql://admin22:12345@localhost:5432/saccrewproject2')
@@ -27,10 +14,8 @@ session = Session(engine)
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-
 # Save reference to the table
 walmartdata = Base.classes.walmartdata
-
 
 app = Flask(__name__)
 
@@ -50,14 +35,12 @@ def jsonifyData():
 def getcategorycounts():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
-# Grab All the data
+    # Grab All the data
     results=session.query(
         walmartdata.category,
         func.count(walmartdata.item_number)
         ).\
         group_by(walmartdata.category)
-
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_sqldata
--- static/boxAndWhisker.js
+++ static/boxAndWhisker.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     var query = 'http://127.0.0.1:5000/api/datacall';
 
+    // Function to find the min, q1, q3, max, and median for a given array
     function QuartCalc(d) {
         q1 = d3.quantile(d.map(function(g) { return g;}).sort(d3.ascending),.25)
         median = d3.quantile(d.map(function(g) { return g;}).sort(d3.ascending),.5)
@@ -13,6 +14,7 @@ window.onload = function () {
 
     // Import Data
     d3.json(query).then(function(data) {
+        // Find unique category values
         function UniqueVal(item, index, self) {
             return self.indexOf(item) === index;
           }
@@ -22,21 +24,18 @@ window.onload = function () {
             AllCategories.push(data[i].category);
         }
         categories = AllCategories.filter(UniqueVal);
-        console.log(categories);
 
-        
-    var plotData = []
-    var  outliers = []
-    for (let i = 0; i < categories.length ; i++) {
-        var dataDict = {};
-        categoryPriceChange = [];
-       
-        
-        for (let j = 0; j < data.length; j++){
-            if (data[j].category == categories[i]){
-                categoryPriceChange.push(data[j].price_percent_change);
-            }    
-          };
+        // Array to hold dictionaries of percent price changes and outliers
+        var plotData = []
+        var  outliers = []
+        for (let i = 0; i < categories.length ; i++) {
+            var dataDict = {};
+            categoryPriceChange = [];
+            for (let j = 0; j < data.length; j++){
+                if (data[j].category == categories[i]){
+                    categoryPriceChange.push(data[j].price_percent_change);
+                }    
+            };
         dataDict['x'] = i;
         dataDict["label"] = categories[i];
         dataDict['y'] = QuartCalc(categoryPriceChange);
@@ -48,15 +47,7 @@ window.onload = function () {
             };
         };
         };
-
-
-
-
-
-    
-console.log(plotData);
-
-
+    // format chart
     var options = {
         animationEnabled: true,
         theme: "dark2", // "light1", "light2", "dark1", "dark2"
@@ -77,22 +68,22 @@ console.log(plotData);
             itemclick: toggleDataSeries
         },
 
-        
+        // build chart
         data: [{
             type: "boxAndWhisker",
             toolTipContent: "<span style=\"color:#6D78AD\">{label}:</span> <br><b>Upper Extreme:</b> {y[3]},<br><b>Q3:</b> {y[2]},<br><b>Median:</b> {y[4]}<br><b>Q1:</b> {y[1]}<br><b>Lower Extreme:</b> {y[0]}",
             dataPoints: plotData,         
                 
         },
-        // {
-        //     type: "scatter",
-        //     name: "Outlier Values",
-        //     color: "#F15278",
-        //     markerType: "circle",
-        //     toolTipContent: "<span style=\"color:#C0504E\">{county}</span>: {y} Days",
-        //     showInLegend: true,
-        //     dataPoints:  outliers
-        // }
+        {
+            type: "scatter",
+            name: "Outlier Values",
+            color: "#F15278",
+            markerType: "circle",
+            toolTipContent: "<span style=\"color:#C0504E\">{county}</span>{y} %",
+            showInLegend: true,
+            dataPoints:  outliers
+        }
     ]
 
     };
